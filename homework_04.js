2dd88c83f0f8668c559966ac878b72ad67e384fe
Mattia Contin <m.contin.dev@gmail.com>
2015-11-20 11:17:50 +0100

    commit

---
+++ homework_04.js
@@ -0,0 +1,17 @@
+function ex_1_F(n){
+    var quoz = 0;
+    var resto = 0;
+    var temp = n;
+    var binary = [];
+    do{
+        resto = temp%2;
+        temp /= 2;
+        quoz = Math.floor(temp);
+        binary.push(resto);     
+    }while(quoz!=0);
+    var res = "";
+    while(binary.length>0){
+        res+=binary.pop();
+    }
+    return res;
+}
